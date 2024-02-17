1d37c6ed7e964c0ffd0c4ae21aff5da0d0a100fd
faguilar <franciscoaguilarvergara@gmail.com>
2024-02-17 12:45:41 -0300

    Subiendo los ultimos cambios

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,8 @@
-const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
 
 $(document).ready(function () {
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
     $("#reservar").click(function () {
         alert("Reserva efectuada correctamente")
 
