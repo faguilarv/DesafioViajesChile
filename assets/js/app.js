b1dcb006af770b9899e4746cc891da0573ad5561
faguilar <franciscoaguilarvergara@gmail.com>
2024-02-17 10:59:45 -0300

    se genera footer-se agrega py-5 para relleno en las secciones

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,18 +2,18 @@ const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
 $(document).ready(function () {
-    $(reservar).click(function () {
+    $("#reservar").click(function () {
         alert("Reserva efectuada correctamente")
 
     });
 
-    $(document).ready(function () {
-        $(registro).click(function () {
-            alert("Check-in de viaje efectuado correctamente")
-        });
+    $("#registro").click(function () {
+        alert("Check-in de viaje efectuado correctamente")
+    });
 
-
-
-
+    $("#send").click(function () {
+        alert("Mensaje enviado correctamente")
     });
 });
+
+
